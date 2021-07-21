9254e30c578d85b7cefbc713925460aeb5eb9f1e
daniapm <daniajibellis@hotmail.com>
2021-07-21 11:32:25 -0500

    correcting  conditional

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -8,7 +8,9 @@
 
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if (node->parent != NULL || node == NULL)
+	if (node == NULL)
+		return (0);
+	if (node->parent != NULL)
 		return (0);
 	return (1);
 }
