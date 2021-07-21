666a5694f6f52587100f7fa4841acc87f1534fc9
daniapm <daniajibellis@hotmail.com>
2021-07-21 11:05:57 -0500

    pep8 style

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,12 +1,23 @@
 #include "binary_trees.h"
-#include <stdio.h>
-#include <stdlib.h>
+
+/**
+ * binary_tree_node - Entry Point
+ * @parent: variable
+ * @value: variable
+ * Return: 0
+ */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *nodo = malloc(sizeof(binary_tree_t));
-    // Asignar el dato e iniciar hojas
-    nodo->parent = parent;
+
+	binary_tree_t *nodo;
+
+	nodo = malloc(sizeof(binary_tree_t));
+	if (nodo == NULL)
+		return (NULL);
+	/*Asignar el dato e iniciar hojas*/
+	nodo->parent = parent;
 	nodo->n = value;
-    return nodo;
+	nodo->left = nodo->right = NULL;
+	return (nodo);
 }
