faf6f771b3a7a315d6f6a963451c60f7e2497f88
SebastianMH14 <sebasmh2002@hotmail.com>
2021-07-22 15:18:56 -0500

    fix task 17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -13,6 +13,6 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 
 	if (node == node->parent->left)
 		return (node->parent->right);
-
-	return (node->parent->left);
+	else
+		return (node->parent->left);
 }
