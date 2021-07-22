cde5cfbe21b5ccfcb6119590f738d005620ba043
SebastianMH14 <sebasmh2002@hotmail.com>
2021-07-22 14:23:47 -0500

    fix task 10 add variable

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,16 +8,16 @@
 */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	size_t i;
-	size_t j;
+	size_t i = 0;
+	size_t j = 0;
 
 	if (tree == NULL)
 		return (0);
 
 	for (i = 0; tree->parent != NULL; i++)
 	{
-		i++;
+		j++;
 		tree = tree->parent;
 	}
-	return (i);
+	return (j);
 }
